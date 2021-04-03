268706792fbe604fd9c543e288b4dea984f3fd2a
Sushrut Tabakade <sushrut.tab1998@gmail.com>
2021-04-03 11:33:06 +0530

    created homescreen and product Card

---
+++ client/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+import products from '../products.js'
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h3>Latest products</h3>  
+            <Row>
+                {products.map((product) => (
+                    <Col sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
+                    </Col>
+                ))}
+            </Row> 
+        </>
+    )
+}
+
+export default HomeScreen
