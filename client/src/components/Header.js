f1a880b9921b62057c5aea5e0683ab44ce34f774
Sushrut Tabakade <sushrut.tab1998@gmail.com>
2021-04-05 01:11:20 +0530

    corrected the navigation bar fontSize

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
-
-const brandstyle = {
-    fontSize: "2rem",
-}
-
-const linkstyle = {
-    fontSize: '1.025rem',
-}
+import { navbar, navbarBrand} from './header.css'
 
 const Header = () => {   
     return (
@@ -17,16 +10,16 @@ const Header = () => {
                 <Navbar bg="dark" variant="dark" expand="lg" className='ml auto' collapseOnSelect>
                     <Container>
                         <LinkContainer to='/'>
-                            <Navbar.Brand style={brandstyle} >Sushrut Ecommerce</Navbar.Brand>
+                            <Navbar.Brand style={navbar} >Sushrut Ecommerce</Navbar.Brand>
                         </LinkContainer>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="ml-auto">
                                 <LinkContainer to='/cart'>
-                                    <Nav.Link style={linkstyle}><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                                    <Nav.Link style={navbarBrand}><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
                                 </LinkContainer>
                                 <LinkContainer to='/login'>
-                                    <Nav.Link style={linkstyle} ><i className="fas fa-user"></i>Sign In</Nav.Link>
+                                    <Nav.Link style={navbarBrand} ><i className="fas fa-user"></i>Sign In</Nav.Link>
                                 </LinkContainer>
                             </Nav>
                         </Navbar.Collapse>
